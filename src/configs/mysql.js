0acae351c93b40421662664afec233a6e633bc44
a7cha <akuarung83@gmail.com>
2020-12-03 08:39:35 +0700

    arung

--- src/configs/mysql.js
+++ src/configs/mysql.js
@@ -3,6 +3,6 @@ const mysql = require("mysql2");
 module.exports = mysql.createConnection({
   host: process.env.DB_HOSTNAME || "localhost",
   user: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "asusa442u",
   database: process.env.DB_DATABASE || "ticketing",
 });
